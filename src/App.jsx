58e9f2e3a28b6f702503928804850553bf75373d
Bernard <bado.bernard94@gmail.com>
2022-02-04 10:18:09 +0100

    finishing the FAB

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { FcAbout, FcBusinessman, FcCamera, FcFullTrash } from "react-icons/fc";
+
+import Fab from "./components/FAB";
+
+const actions = [
+  { label: "About", icon: <FcAbout />, onClick: console.log },
+  { label: "Profile", icon: <FcBusinessman />, onClick: console.log },
+  { label: "Picture", icon: <FcCamera />, onClick: console.log },
+  { label: "Trash", icon: <FcFullTrash />, onClick: console.log },
+];
+
 const App = () => {
-  return <main>App</main>;
+  return (
+    <main>
+      <Fab actions={actions} />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/FAB/FAB.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import cn from "classnames";
+import { MdAdd } from "react-icons/md";
+
+import "./styles.scss";
+
+const FAB = ({ actions }) => {
+  const [open, setOpen] = useState(false);
+
+  const mouseEnter = () => setOpen(true);
+
+  const mouseLeave = () => setOpen(false);
+
+  return (
+    <ul
+      className="fab-container"
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+    >
+      <li className="fab-button">
+        <MdAdd />
+      </li>
+      {actions.map((action, index) => (
+        <li
+          style={{ transitionDelay: `${index * 25}ms` }}
+          className={cn("fab-action", { open })}
+          key={action.label}
+          onClick={action.onClick}
+        >
+          {action.icon}
+          <span className="tooltip">{action.label}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FAB;
